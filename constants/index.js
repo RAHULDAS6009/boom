80cb94beaa061c4bf9f110da4ff4f317b166edb5
Rahul Das <das206053@gmail.com>
2024-09-12 17:01:30 +0530

    sideBar initialized

---
+++ constants/index.js
@@ -0,0 +1,27 @@
+export const sideLinks = [
+  {
+    label: "Home",
+    route: "/",
+    icon: "icons/home.svg",
+  },
+  {
+    label: "Upcoming",
+    route: "/upcoming",
+    icon: "/icons/upcoming.svg",
+  },
+  {
+    label: "Previous",
+    route: "/previous",
+    icon: "icons/previous.svg",
+  },
+  {
+    label: "Recordings",
+    route: "/recordings",
+    icon: "icons/recordings.svg",
+  },
+  {
+    label: "Personal Room",
+    route: "/previous-room",
+    icon: "icons/previous-room.svg",
+  },
+];
