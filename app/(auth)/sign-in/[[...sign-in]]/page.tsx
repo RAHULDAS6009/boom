cabb2869c62f803ed44167646bd09dbaf58fcb3d
Rahul Das <das206053@gmail.com>
2024-09-14 12:42:17 +0530

    hero section of Home Page

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const SignInPage = () => {
   return (
-    <main className='w-full h-screen flex-center'>
+    <main className='w-full h-screen flex-center  '>
       <SignIn/>
     </main>
   )
